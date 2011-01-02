4ca4720c72000ac7fb64d48aed73190c2c3c53c2
Tom Parker <palfrey@tevp.net>
2011-01-02 23:45:40 +0000

    Add more tests, including using a proper MP3

--- jukebox/tests.py
+++ jukebox/tests.py
@@ -3,10 +3,27 @@ from django.test.client import Client
 from jsonrpc._json import loads, dumps
 from uuid import uuid1
 from jukebox.models import *
+from time import sleep
+import utils
+from spider import spider
+from cache import downloader
 
 class JukeboxTest(TestCase):
+	static_path = "http://localhost/static/"
+
 	def setUp(self):
-		self.client = Client()
+		utils.client = self.client
+		self._method("rescan_root", self.static_path)
+
+	def needs_static(self):
+		while len(spider.todo())>0:
+			print "spider todo", spider.todo()
+			sleep(.5)
+
+	def needs_downloaded(self):
+		while len(downloader.todo())>0:
+			print "downloader todo", downloader.todo()
+			sleep(.5)
 
 	def _method(self, method, *params):
 		req = {
@@ -18,17 +35,30 @@ class JukeboxTest(TestCase):
 		return self._call(req)
 	
 	def _call(self, req):
-		return loads(self.client.post("/rpc/jukebox", dumps(req), content_type="application/json").content)[u'result']
+		resp = loads(self.client.post("/rpc/jukebox", dumps(req), content_type="application/json").content)
+		self.assert_("result" in resp.keys(), resp)
+		return resp["result"]
 
-	def _addTestTrack(self):
-		url = "http://localhost/"+uuid1().hex
-		m = MusicFile()
-		m.url = url
-		m.save()
+	def _addTestTrack(self, url = None):
+		if url == None:
+			url = "http://localhost/"+uuid1().hex
+		if MusicFile.objects.filter(url = url).count() == 0:
+			m = MusicFile()
+			m.url = url
+			m.save()
+			print "added test track", url
+		else:
+			print "test track already present", url
 		return url
 
 	def _enqueueTestTrack(self):
 		url = self._addTestTrack()
+		resp = self._method("enqueue", "test_user", [{"url":url}], False)
+		return (url, resp)
+
+	def _enqueueRealTrack(self):
+		self.needs_static()
+		url = self._addTestTrack(self.static_path+"silent-3mins.mp3")
 		resp = self._method("enqueue", "test_user", [{"url":url}], False)
 		return (url, resp)
 
@@ -40,8 +70,11 @@ class JukeboxTest(TestCase):
 
 	def testPlay(self): 
 		QueueItem.objects.all().delete() # clear anything else in there
+		res = self._method("pause", False)
+		self.assertEqual(res['status'], "idle", res)
+		self.assertEqual(res['entry'], None, res)
+		self.assertEqual(res['queue'], [], res)
 
-		self._method("pause", False)
 		(url, _) = self._enqueueTestTrack()
 		res = self._method("pause", False)
 		self.assertEqual(res['paused'], False, res)
@@ -50,8 +83,8 @@ class JukeboxTest(TestCase):
 	def testSkip(self): 
 		QueueItem.objects.all().delete() # clear anything else in there
 
-		(url, _) = self._enqueueTestTrack()
-		(url2, _) = self._enqueueTestTrack()
+		(url, _) = self._enqueueRealTrack()
+		(url2, _) = self._enqueueRealTrack()
 
 		res = self._method("get_queue")
 		self.assertEqual(res['entry']['url'], url, res)
@@ -61,14 +94,31 @@ class JukeboxTest(TestCase):
 		self.assertEqual(res['entry'], None, res)
 
 	def testSkipWithPlay(self): 
-		(url, _) = self._enqueueTestTrack()
-		(url2, _) = self._enqueueTestTrack()
+		(url, _) = self._enqueueRealTrack()
+		(url2, _) = self._enqueueRealTrack()
 
 		res = self._method("pause", False)
+		self.assertNotEqual(res['entry'], None, res)
 		self.assertEqual(res['entry']['url'], url, res)
 		self.assertEqual(res['paused'], False, res)
-		self.assertEqual(res['status'], "caching", res)
+		self.needs_downloaded()
+		self.assertEqual(res['status'], "playing", res)
+
 		res = self._method("skip", "test_user")
 		self.assertEqual(res['entry']['url'], url2, res)
+
 		res = self._method("skip", "test_user")
 		self.assertEqual(res['entry'], None, res)
+
+	def testPlay(self): 
+		QueueItem.objects.all().delete() # clear anything else in there
+
+		res = self._method("pause", False)
+		self.assertEqual(res['entry'], None, res)
+		(url, _) = self._enqueueRealTrack()
+		res = self._method("pause", False)
+		self.needs_downloaded()
+		self.assertEqual(res['paused'], False, res)
+		self.assertEqual(res['status'], "playing", res)
+
+

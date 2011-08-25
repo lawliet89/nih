f6b95082dd9e56c65b4a4413dfce389e7ed64693
Tom Parker <palfrey@tevp.net>
2011-08-25 17:42:59 +0100

    Cope with spidering items that are already deleted

--- jukebox/spider.py
+++ jukebox/spider.py
@@ -3,6 +3,7 @@ from BeautifulSoup import BeautifulSoup
 from utils import urlopen, HTTPError, BackgroundTask, registerStartupTask
 from urlparse import urljoin
 from os.path import splitext
+from django.core.exceptions import ObjectDoesNotExist
 
 known_extensions = [".mp3", ".ogg", ".flac", ".wma", ".m4a"]
 
@@ -19,29 +20,38 @@ class Spider(BackgroundTask):
 			current.failed = True
 			current.save()
 			return
-		url = page.geturl()
-		soup = BeautifulSoup(page)
-	
-		for link in soup.findAll("a"):
-			resolved = urljoin(url, link["href"])
-			if len(resolved) < len(url): # up link, skip
-				print "skipping",resolved, url
-				continue
-			if resolved[-1] == "/": # directory
-				if WebPath.objects.filter(url=resolved).count() == 0:
-					child = current.add_child(url=resolved)
-					self.add(child)
-			else: # file?
-				(_, ext) = splitext(resolved)
-				ext = ext.lower()
-				if ext in known_extensions:
-					if MusicFile.objects.filter(url=resolved).count() == 0:
-						mf = MusicFile(parent=current, url = resolved)
-						mf.save()
-				else:
-					print "Can't handle", resolved, ext, len(ext)
 
-		current.checked = True
-		current.save()
+		try:
+			url = page.geturl()
+			soup = BeautifulSoup(page)
+		
+			for link in soup.findAll("a"):
+				try:
+					resolved = urljoin(url, link["href"])
+				except KeyError:
+					print "skipping due to lack of href", link
+					continue
+				if len(resolved) < len(url): # up link, skip
+					print "skipping",resolved, url
+					continue
+				if resolved[-1] == "/": # directory
+					if WebPath.objects.filter(url=resolved).count() == 0:
+						child = current.add_child(url=resolved)
+						self.add(child)
+				else: # file?
+					(_, ext) = splitext(resolved)
+					ext = ext.lower()
+					if ext in known_extensions:
+						if MusicFile.objects.filter(url=resolved).count() == 0:
+							mf = MusicFile(parent=current, url = resolved)
+							mf.save()
+					else:
+						print "Can't handle", resolved, ext, len(ext)
+
+			current.checked = True
+			current.save()
+		except ObjectDoesNotExist:
+			# we got deleted
+			pass
 
 spider = registerStartupTask(Spider)

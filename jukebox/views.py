dcb83250f41228b5c865fea3930fb7312c7842bb
Tom Parker <palfrey@tevp.net>
2011-11-14 23:53:11 +0000

    Fix #24, adding last.fm/github links

--- jukebox/views.py
+++ jukebox/views.py
@@ -1,9 +1,10 @@
 from django_genshi import render_to_response
+from django.conf import settings
 from spider import spider
 from models import *
 
 def index(request):
-	return render_to_response("index.xml")
+	return render_to_response("index.xml",{"lastfm_name": settings.LASTFM_USER } )
 
 def spider(request):
 	return render_to_response("spider.xml", {"spider": spider})

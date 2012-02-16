0d9d8885c98ba2c2fdd8a33bbf4b25c3ae30da2c
Tom Parker <palfrey@tevp.net>
2012-02-16 14:39:31 +0000

    Pip needs install adding to command line...

--- generate-virtualenv-bootstrap.py
+++ generate-virtualenv-bootstrap.py
@@ -5,7 +5,7 @@ def after_install(options, home_dir):
     etc = join(home_dir, 'etc')
     if not os.path.exists(etc):
         os.makedirs(etc)
-    subprocess.call([join(home_dir, 'bin', 'pip'), 'django'])
-    subprocess.call([join(home_dir, 'bin', 'pip'), 'psycopg2'])
+    subprocess.call([join(home_dir, 'bin', 'pip'), 'install', 'django'])
+    subprocess.call([join(home_dir, 'bin', 'pip'), 'install', 'psycopg2'])
 """))
 f = open('bootstrap.py', 'w').write(output)

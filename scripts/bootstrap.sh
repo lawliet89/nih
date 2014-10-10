f384ef813c7ca8831c7376b7eb569abd972cc020
Martin Eden <martin@lshift.net>
2014-10-10 22:04:56 +0100

    Use .gitmodules to get associated source, not handcrafted scripts

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 set -e
+git submodule update --init --recursive
 apt-get install python-pip python-musicbrainz2 python-alsaaudio python-magic python-mysqldb mysql-server python-gst0.1
 pip install django==1.3.1 django-nose South mutagen BeautifulSoup django-genshi
-
-pushd src/ext
-rm -r pyscrobbler
-rm -r django-json-rpc
-git clone git://github.com/offmessage/pyscrobbler.git
-git clone git://github.com/palfrey/django-json-rpc.git
-cd django-json-rpc
-python setup.py install
-popd
 
 python scripts/setupdb.py
 echo ""

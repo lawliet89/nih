f6ba8844b600353f11320aa0bf0a9440b194ec14
Martin Eden <martin@lshift.net>
2014-11-17 15:12:13 +0000

    Dynamically update the version timestamp

--- src/jukebox/static/viewmodels/version.js
+++ src/jukebox/static/viewmodels/version.js
@@ -3,9 +3,10 @@ function VersionViewModel() {
     this.hash = ko.observable();
     this.timestamp = ko.observable();
     this.url = ko.observable();
+    this.now = ko.observable(new Date());
 
     this.when = ko.computed(function() {
-        return moment(this.timestamp()).fromNow();
+        return moment(this.timestamp()).from(this.now());
     }, this);
 }
 VersionViewModel.prototype.setup = function() {
@@ -17,4 +18,7 @@ VersionViewModel.prototype.updateVersion = function(version) {
     this.hash(version.hash);
     this.timestamp(version.timestamp);
     this.url(version.url);
+    // Every minute, rerender the timestamp
+    var me = this;
+    setInterval(function() { me.now(new Date()); }, 60 * 1000);
 }
